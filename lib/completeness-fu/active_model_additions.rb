f966c189fc99c7eb6849bc080f142b9f287c6bb4
Davide <malagoli@gmail.com>
2013-09-30 20:54:08 +0200

    added demodulize in order to correctly manage translation for scoped models

--- lib/completeness-fu/active_model_additions.rb
+++ lib/completeness-fu/active_model_additions.rb
@@ -97,7 +97,7 @@ module CompletenessFu
         end
 
         def translate_check_details(full_check)
-          namespace = CompletenessFu.default_i18n_namespace + [self.class.model_name.underscore.to_sym, full_check[:name]]
+          namespace = CompletenessFu.default_i18n_namespace + [self.class.model_name.demodulize.underscore.to_sym, full_check[:name]]
 
           translations = [:title, :description, :extra].inject({}) do |list, field|
                            list[field] = I18n.t(field.to_sym, :scope => namespace)
